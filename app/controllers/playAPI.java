613fe380aaa893f2fb10ac665a4f386b83050ace
piardi <piardi@upmc>
2015-03-05 11:52:04 +0100

    added playAPI changed library settings

---
+++ app/controllers/playAPI.java
@@ -0,0 +1,12 @@
+package controllers;
+
+/**
+ * This class is used to create the API 
+ * System architecture:
+ * iOS <-(web)-> [API <-> System <-> DAO] <-> DataBase
+ * @author piardi
+ *
+ */
+public class playAPI {
+	
+}
