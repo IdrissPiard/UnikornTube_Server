05b42be41d52ec61006d25d8107634eb19e43d8e
guiguileguigui <guijja@gmail.com>
2015-03-31 16:08:27 +0200

    giveReseenVideo
    Recommandation des vidéos de l'historique

--- app/models/recommandation/FrontRecommandation.java
+++ app/models/recommandation/FrontRecommandation.java
@@ -1,11 +1,54 @@
 package models.recommandation;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import utils.bdd.MysqlConnection;
 
 public class FrontRecommandation {
 	
+	/**
+	 * Renvoit une liste d'id vidéo recommandés en fonction d'une vidéo donnée
+	 * @param parIdVideo > id de la vidéo 
+	 * @return une liste d'id video recommandé
+	 */
 	public static ArrayList <Integer> giveRecommandation ( int parIdVideo )
 	{
 		return Level1BDD.giveRecommandation ( parIdVideo ) ;
+	}
+	
+	/**
+	 * Renvoit une liste d'id vidéo de vidéo d'une rubrique : " Regarder à nouveau " 
+	 * @param parIdVideo > id de la vidéo 
+	 * @return une liste d'id ( entre 0 et 5 ) video recommandé Regarder à nouveau
+	 */
+	public static ArrayList <Integer> giveReseenVideo( int parIDUser)
+	{
+		ArrayList <Integer> locListRecommandation = new ArrayList <Integer> ();
+		
+		 try {
+				ResultSet resultSet = MysqlConnection.executeQuery( "SELECT `id_video`"
+						+ " FROM `likes`"
+						+ " WHERE `id_user` = "+parIDUser+" AND `value` != 2"
+						+ " ORDER BY `viewedtime`"
+						+ " LIMIT 0 , 5 " ) ;
+				
+				while(resultSet.next()){
+					int anID = resultSet.getInt(1) ;
+					if (locListRecommandation.contains(anID) == false)
+					{
+						locListRecommandation.add(anID);
+					}
+				}
+				
+				resultSet.close();
+				
+			} catch (SQLException e) {
+				//TODO LOG
+				e.printStackTrace();
+			}
+		
+		return locListRecommandation;
 	}
 }
