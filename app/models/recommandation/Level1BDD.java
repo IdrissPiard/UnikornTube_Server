3ddb5a303829689c4e69861ca670c84340a87fb0
guiguileguigui <guijja@gmail.com>
2015-03-31 15:36:04 +0200

    Lvl1 Recommandation ok
    Recommandation au niveau 1 ok

--- app/models/recommandation/Level1BDD.java
+++ app/models/recommandation/Level1BDD.java
@@ -1,33 +1,118 @@
 package models.recommandation;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import utils.bdd.MysqlConnection;
 
 class Level1BDD {
 	
 	static ArrayList <Integer> giveRecommandation ( int parIdVideo )
 	{
-		ArrayList <Integer> locListRecommandation = new ArrayList <Integer> ();
+		ArrayList <Integer> locListRecommandationTotal = new ArrayList <Integer> ();
 		
-		locListRecommandation.addAll( recommandationByPlaylist(parIdVideo) ) ;
-		locListRecommandation.addAll( recommandationByTag(parIdVideo) ) ;
+		locListRecommandationTotal.addAll( recommandationByPlaylist(parIdVideo) ) ;
+		locListRecommandationTotal.addAll( recommandationByTag(parIdVideo) ) ;
 		
-		return locListRecommandation ;
+		ArrayList <Integer> locListRecommandationGood = new ArrayList <Integer> ();
+		
+		for (Integer integer : locListRecommandationTotal) {
+			if ( locListRecommandationGood.contains(integer) == false)
+			{
+				locListRecommandationGood.add(integer);
+			}
+		}
+		
+		return locListRecommandationGood ;
 	}
 	
-	static ArrayList <Integer> recommandationByPlaylist ( int parIdVideo )
+	static ArrayList <Integer> recommandationByTag ( int parIdVideo )
 	{
 		ArrayList <Integer> locListRecommandation = new ArrayList <Integer> ();
 		
-		// TODO Ask playlist
+		try {
+			
+			ArrayList <Integer> locListTagCorrect = new ArrayList <Integer> ();
+			
+			ResultSet resultSet = MysqlConnection.executeQuery( "SELECT `id` , `tag` "
+					+ " FROM `tags` "
+					+ " WHERE `id` in ( SELECT DISTINCT `id_tag` "
+					+ " FROM `tag_video`"
+					+ " WHERE `id_video` = 10 ) ") ;
+			ResultSet resultSet2 = null ;
+			while(resultSet.next()){
+				locListTagCorrect.add( resultSet.getInt(1) );
+				
+				resultSet2 = MysqlConnection.executeQuery( "SELECT `id_video`"
+						+ " FROM `tag_video`"
+						+ " WHERE `id_tag` IN ("
+						+ " SELECT DISTINCT `id` FROM `tags` WHERE `tag` LIKE '%"+resultSet.getString(2)+"%'"
+						+ ") " );
+				while(resultSet2.next()){
+					int anID = resultSet2.getInt(1) ;
+					if (locListRecommandation.contains(anID) == false)
+					{
+						locListRecommandation.add(anID);
+					}
+				}
+				resultSet2.close();
+				
+			}
+			resultSet.close();
+			
+			if (locListTagCorrect.isEmpty() == false)
+			{
+				String tag = locListTagCorrect.get(0) + "";
+				
+				for (int i = 1; i < locListTagCorrect.size(); i++) {
+					tag = "," + locListTagCorrect.get(i) ;
+				}
+							
+				resultSet2 = MysqlConnection.executeQuery( "SELECT `id_video` FROM `tag_video` WHERE `id_tag` in ( "+tag+" ) ") ;
+				
+				while(resultSet2.next()){
+					int anID = resultSet2.getInt(1) ;
+					if (locListRecommandation.contains(anID) == false)
+					{
+						locListRecommandation.add(anID);
+					}
+				}
+				resultSet2.close();
+				
+			}
+			
+		} catch (SQLException e) {
+			//TODO LOG
+			e.printStackTrace();
+		}
 		
 		return locListRecommandation ;
 	}
 	
-	 static ArrayList <Integer> recommandationByTag ( int parIdVideo )
+	 static ArrayList <Integer> recommandationByPlaylist ( int parIdVideo )
 	{
 		 ArrayList <Integer> locListRecommandation = new ArrayList <Integer> ();
 		 
-		// TODO Ask Tag
+		 try {
+			ResultSet resultSet = MysqlConnection.executeQuery( "SELECT `id_video` FROM `playlist_video` WHERE `id_playlist` in ( SELECT DISTINCT `id_playlist` FROM `playlist_video` WHERE `id_video` = "+parIdVideo+") and `id_video` <> "+parIdVideo+"" ) ;
+			
+			while(resultSet.next()){
+				int anID = resultSet.getInt(1) ;
+				
+				if (locListRecommandation.contains(anID) == false)
+				{
+					locListRecommandation.add(anID);
+				}
+				
+			}
+			
+			resultSet.close();
+			
+		} catch (SQLException e) {
+			//TODO LOG
+			e.printStackTrace();
+		}
 		 
 		 return locListRecommandation ;
 	}
--- app/utils/bdd/MysqlConnection.java
+++ app/utils/bdd/MysqlConnection.java
@@ -9,7 +9,7 @@ import java.sql.Connection;
 import utils.AppOptionsHelper;
 
 /**
- * 
+ * Dao
  * @author Guigui
  *
  */
